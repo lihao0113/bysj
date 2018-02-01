3856d84576dab10659ec88b9c427fbdd72874895
lihao <963106487@qq.com>
2018-02-01 10:08:04 +0800

    文件列表

--- src/main/java/cn/mystic/controller/FileController.java
+++ src/main/java/cn/mystic/controller/FileController.java
@@ -1,14 +1,17 @@
 package cn.mystic.controller;
 
+import cn.mystic.service.FileService;
+import cn.mystic.utils.OutputUtil;
+import com.alibaba.fastjson.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 
@@ -18,36 +21,22 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/file")
 public class FileController {
+    @Autowired
+    private FileService fileService;
 
     /**
      * 实现文件上传
-     * */
-    @RequestMapping(value="upload",method = RequestMethod.POST)
+     */
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
     @ResponseBody
-    public String fileUpload(MultipartFile file){
-
-        if(file.isEmpty()){
-            return "false";
-        }
-        String fileName = file.getOriginalFilename();
-
-        String path = System.getProperty("user.dir") + "/uploadFile" ;
-        File dest = new File(path + "/" + fileName);
-        if(!dest.getParentFile().exists()){ //判断文件父目录是否存在
-            dest.getParentFile().mkdir();
-        }
-        try {
-            file.transferTo(dest); //保存文件
-            return "true";
-        } catch (IllegalStateException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return "false";
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return "false";
-        }
+    public String fileUpload(MultipartFile file) {
+        return fileService.fileUpload(file);
     }
+    @RequestMapping(value = "/fileList", method = RequestMethod.GET)
+    public void findAll(HttpServletRequest request, HttpServletResponse response) {
+        JSONObject res = fileService.getFileList();
+        OutputUtil.print(response,res);
+    }
+
 
 }
--- src/main/java/cn/mystic/service/FileService.java
+++ src/main/java/cn/mystic/service/FileService.java
@@ -1,27 +1,65 @@
 package cn.mystic.service;
 
-import cn.mystic.domain.SysUser;
-import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileItemFactory;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.InputStream;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by lihao on 2018/1/31.
  */
 @Service
 public class FileService {
-    public JSONObject upload(HttpServletRequest request, HttpServletResponse response) {
+
+    public String fileUpload(MultipartFile file) {
+        if (file.isEmpty()) {
+            return "false";
+        }
+        String fileName = file.getOriginalFilename();
+
+        String path = System.getProperty("user.dir") + "/uploadFile";
+        File dest = new File(path + "/" + fileName);
+        if (!dest.getParentFile().exists()) { //判断文件父目录是否存在
+            dest.getParentFile().mkdir();
+        }
+        try {
+            file.transferTo(dest); //保存文件
+            return "上传成功";
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+            return "上传失败";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "上传失败";
+        }
+    }
+
+    public JSONObject getFileList() {
         JSONObject result = new JSONObject();
+        ArrayList<String> filelist = new ArrayList<String>();
+        String path = System.getProperty("user.dir") + "/uploadFile";
+        filelist = getFiles(path);
+        result.put("data", filelist);
         return result;
     }
 
+    static ArrayList<String> getFiles(String filePath) {
+        ArrayList<String> filelist = new ArrayList<String>();
+        File root = new File(filePath);
+        File[] files = root.listFiles();
+        for (File file : files) {
+//            if (file.isDirectory()) { // 递归调用
+//                getFiles(file.getAbsolutePath());
+//                filelist.add(file.getName());
+//                System.out.println("显示" + filePath + "下所有文件夹" + file.getName());
+//            } else {
+            filelist.add(file.getName());
+            System.out.println("显示" + filePath + "下所有文件名" + file.getName());
+//            }
+        }
+        return filelist;
+    }
 }
